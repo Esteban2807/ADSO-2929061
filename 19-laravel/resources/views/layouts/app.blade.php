d95bdf7d4c7f5afd5b6a42fae87401d3273cb120
Esteban2807 <juanmua2007@gmail.com>
2026-03-17 21:12:28 -0500

    Hoy hicé mas cambios que nunca

---
+++ 19-laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/daisyui5.css') }}" rel="stylesheet" type="text/css"/>
+    <title>@yield('title')</title>
+</head>
+@auth
+@php
+(Auth::user()->role == 'Customer') ? $colors = 'blue,bluesky' : $colors = 'green,lime'
+@endphp
+@else
+    @php
+        $colors = 'black,transparent'
+    @endphp
+@endauth
+<body class="bg-[linear-gradient(to_top,{{ $colors }})] bg-[url({{asset('images/backgroundd.jpg')}})] min-h-dvh bg-no-repeat bg-center bg-cover pt-14 bg-fixed">
+    <main class="p-12 flex flex-col gap-2 justify-center items-center min-h-dvh">
+        
+            @yield('content')
+    </main>
+        <script src="{{asset('js/tailwindcss4.js')}}"></script>
+</body>
+
+</html>
--- 19-laravel/routes/web.php
+++ 19-laravel/routes/web.php
@@ -1,59 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('helloworld', function () {
-    return '<h1>Hello World! 👻</h1>';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::middleware('auth')->group(function () {
+    Route::resources([
+        'users' => UserController::class,
+    ]);
 });
 
-Route::get('getallpets', function () {
-    $pets = \App\Models\Pet::take(10)->get();
-    dd($pets->toArray());
-});
 
-Route::get('sayhello/{name}', function ($name) {
-    return '<h1>Hello  '  . $name . '! 👻</h1>';
-});
-
-Route::get('getall/showpet/{id}', function () {
-    $pet = \App\Models\Pet::find(request()->id);
-    dd($pet->toArray());
-});
-
-Route::get('getall/users', function () {
-    $users = \App\Models\User::get();
-    echo '<table border="1">' .
-        '<tr>' .
-        '<td>Document</td>' .
-        '<td>Fullname</td>' .
-        '<td>Gender</td>' .
-        '<td>Birthdate</td>' .
-        '<td>Age</td>' .
-        '<td>Photo</td>' .
-        '<td>Phone</td>' .
-        '<td>Email</td>' .
-        '<td>Active</td>' .
-        '<td>Role</td>' .
-        '</tr>';
-    foreach ($users as $user) {
-        echo 
-        '<tr>' .
-        '<td>' . $user->document . '</td>' .
-        '<td>' . $user->fullname . '</td>' .
-        '<td>' . $user->gender . '</td>' .
-        '<td>' . $user->birthdate . '</td>' .
-        '<td>' . (new DateTime($user->birthdate))->diff(new DateTime())->y . '</td>' .
-        '<td> <img src="' . asset($user->photo) . '" alt="Photo" width="50" height="50"></td>' .
-        '<td>' . $user->phone . '</td>' .
-        '<td>' . $user->email . '</td>' .
-        '<td>' . ($user->active == 1 ? 'Yes' : 'No') . '</td>' .
-        '<td>' . $user->role . '</td>' .
-        '</tr>' ;
-        
-    }
-    echo '</table>';
-});
+require __DIR__.'/auth.php';
